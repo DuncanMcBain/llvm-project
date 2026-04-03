f7bcbbc3d18f641b0512292e5bb27472c64bd975
Duncan McBain <dmcbain@modular.com>
2026-04-03 10:39:56 +0100

    Go back to the prev way of handling steps

--- lldb/source/Plugins/Process/Utility/StopInfoMachException.cpp
+++ lldb/source/Plugins/Process/Utility/StopInfoMachException.cpp
@@ -831,7 +831,7 @@ void StopInfoMachException::PerformAction([[maybe_unused]] Event *event_ptr) {
   if (!(m_value == 6 /*EXC_BREAKPOINT*/ &&
         m_exc_code == 1 /*EXC_ARM_BREAKPOINT*/))
     return;
-  SkipOverTrapInstruction();
+  //SkipOverTrapInstruction();
 }
 
 // Detect an unusual situation on Darwin where:
